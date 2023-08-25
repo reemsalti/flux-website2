9fd86a5edc9a37bfe59d838a7cc8fbf1f3fe0982
Reem Salti <reem@Reems-MacBook-Pro.local>
2023-08-25 17:55:54 -0400

    removed hashrouter

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,18 +10,18 @@ export const App: VFC =  () => {
 
 	return (
 		<>
-		<HashRouter>
+		
 		<div className='wrapper' style={{ position: 'absolute', width: '100vw', height: '100vh', top:'0', alignItems: 'center', justifyContent: 'center', display: 'flex' }}>
 		<div className='wrapper' style={{ position: 'absolute', width: '100vw', height: '100vh', top:'0', alignItems: 'center', justifyContent: 'center', display: 'flex' }}></div>
 		<img style={{display: 'flex', position: "absolute", width: "35rem", height: "30rem", zIndex: '1', justifyContent: 'center', alignItems: 'center'}} src={fluxLogo} alt=''/>
 		<p style={{fontFamily: 'Aldrich', display: 'flex', position: 'absolute', zIndex: "2", color: '#fff', fontSize: '3rem', bottom: '10%'}}>COMING SOON</p>
 		<div/>
 			<Leva hidden/>
-			<Routes>
-				<Route path={"/"} element={<TCanvas/>}/>
-			</Routes>
+			
+				<TCanvas/>
+			
 		</div>
-		</HashRouter>
+		
 		</>
 	)
 }
